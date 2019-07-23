b6d15a6c1c22d6616c7b498a062f5c8fc5892ccf
cristian <scremato@libero.it>
2019-07-23 10:47:33 +0200

    segundo comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InformacionService } from "./services/informacion.service";
-
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
+  ngOnInit() {
+    AOS.init();
+  }
   constructor ( public _is:InformacionService ){
 
 
